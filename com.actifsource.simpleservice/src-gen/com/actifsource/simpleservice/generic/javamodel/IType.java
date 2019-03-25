d458ec3905200d2f49fa76660aa1fc0002ac1aa1
CARLOS HENRIQUE ARYPE ILHA <15104291@pucrs.br>
2019-03-25 20:47:33 -0300

    piemo comit

---
+++ com.actifsource.simpleservice/src-gen/com/actifsource/simpleservice/generic/javamodel/IType.java
@@ -0,0 +1,11 @@
+package com.actifsource.simpleservice.generic.javamodel;
+
+import ch.actifsource.util.collection.IMultiMapOrdered;
+
+public interface IType extends ch.actifsource.core.javamodel.INamedResource {
+
+  public static final ch.actifsource.core.INode TYPE_ID = new ch.actifsource.core.Resource("26066078-4f54-11e9-8a71-0dc1bad01b6f");
+  
+}
+
+/* Actifsource ID=[3ca9f967-db37-11de-82b8-17be2e034a3b,26066078-4f54-11e9-8a71-0dc1bad01b6f,Qiailo69m3WfsmCD0V6NO54zK8E=] */
---
+++ com.actifsource.simpleservice/src-gen/com/actifsource/simpleservice/specific/SpecificPackage.java
@@ -0,0 +1,26 @@
+package com.actifsource.simpleservice.specific;
+
+import ch.actifsource.core.Resource;
+
+public class SpecificPackage {
+  
+  /* Begin Protected Region [[user]] */
+  
+  /* End Protected Region   [[user]] */
+  
+  private SpecificPackage() {}
+  
+  public static final String getScopeName() {
+    return "com.actifsource.simpleservice";
+  }
+  
+  public static final String getResourceFolderPath() {
+    return "asrc";
+  }
+  
+  public static final String getPackageName() {
+    return "com.actifsource.simpleservice.specific";
+  }
+  
+}
+/* Actifsource ID=[c1a07b92-fc09-11e0-b756-0dc1d96a76cb,com.actifsource.simpleservice.specific,yX90xNbYaXGg4UqeRGIoEkDogHY=] */
